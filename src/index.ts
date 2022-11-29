776f9150af661dab63a6e0dd64aa151f6cf9df8a
Taylor Romero <me@taylorrome.ro>
2022-11-29 10:19:44 -0700

    minor: exporting buildPermissionContractId

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as plugin } from './permission.plugin'
 export * from './permission.types'
+export { default as buildPermissionContractId } from './utilities/buildPermissionContractId'
