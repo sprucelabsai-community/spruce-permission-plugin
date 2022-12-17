38c6d7d63a21809efd31ee73c1507f4135b074e4
Taylor Romero <me@taylorrome.ro>
2022-12-17 12:37:53 -0700

    minor: AuthorizerFactory now has a singleton available across the stack

--- src/__tests__/behavioral/AuthorizerFactory.test.ts
+++ src/__tests__/behavioral/AuthorizerFactory.test.ts
@@ -2,11 +2,17 @@ import { Authorizer } from '@sprucelabs/heartwood-view-controllers'
 import { PermissionContractId, PermissionId } from '@sprucelabs/mercury-types'
 import { fake } from '@sprucelabs/spruce-test-fixtures'
 import { test, assert, errorAssert } from '@sprucelabs/test-utils'
+import AuthorizerImpl from '../../authorizer/Authorizer'
 import AuthorizerFactory from '../../authorizer/AuthorizerFactory'
 import AbstractPermissionTest from '../support/AbstractPermissionTest'
 
 @fake.login()
 export default class AuthorizerFactoryTest extends AbstractPermissionTest {
+	protected static async beforeEach(): Promise<void> {
+		await super.beforeEach()
+		AuthorizerFactory.reset()
+	}
+
 	@test()
 	protected static async throwsWhenMissingRequired() {
 		const err = await assert.doesThrowAsync(() =>
@@ -24,6 +30,35 @@ export default class AuthorizerFactoryTest extends AbstractPermissionTest {
 		AuthorizerFactory.setClass(StubAuthorizer)
 		const auth = AuthorizerFactory.Authorizer(() => this.mercury.connectToApi())
 		assert.isTrue(auth instanceof StubAuthorizer)
+	}
+
+	@test()
+	protected static async cantGetSharedInstanceWithoutSettingConnectionFactory() {
+		const err = assert.doesThrow(() => AuthorizerFactory.getInstance())
+		errorAssert.assertError(err, 'MISSING_PARAMETERS', {
+			parameters: ['connectToApi'],
+		})
+	}
+
+	@test()
+	protected static async canGetSharedInstanceWhenFactoryIsSet() {
+		this.setupFactory()
+		const auth1 = AuthorizerFactory.getInstance()
+		assert.isTrue(auth1 instanceof AuthorizerImpl)
+	}
+
+	@test()
+	protected static async returnsSameInstance() {
+		const auth1 = AuthorizerFactory.getInstance()
+		const auth2 = AuthorizerFactory.getInstance()
+		assert.isEqual(auth1, auth2)
+		AuthorizerFactory.reset()
+		const auth3 = AuthorizerFactory.getInstance()
+		assert.isNotEqual(auth2, auth3)
+	}
+
+	private static setupFactory() {
+		AuthorizerFactory.setConnectToApi(() => this.mercury.connectToApi())
 	}
 }
 
--- src/authorizer/AuthorizerFactory.ts
+++ src/authorizer/AuthorizerFactory.ts
@@ -5,12 +5,34 @@ import AuthorizerImpl from './Authorizer'
 
 export default class AuthorizerFactory {
 	private static Class: AuthorizerContructor = AuthorizerImpl
+	private static connectToApi: MercuryConnectFactory
+	private static instance?: Authorizer
+
 	public static setClass(Class: AuthorizerContructor) {
 		this.Class = Class
 	}
+
+	public static reset() {
+		this.Class = AuthorizerImpl
+		this.instance = undefined
+	}
+
 	public static Authorizer(connectToApi: MercuryConnectFactory) {
 		assertOptions({ connectToApi }, ['connectToApi'])
 		return new this.Class(connectToApi)
+	}
+
+	public static getInstance(): Authorizer {
+		if (this.instance) {
+			return this.instance
+		}
+		assertOptions({ connectToApi: this.connectToApi }, ['connectToApi'])
+		this.instance = this.Authorizer(this.connectToApi)
+		return this.instance
+	}
+
+	public static setConnectToApi(connectToApi: MercuryConnectFactory) {
+		this.connectToApi = connectToApi
 	}
 }
 
